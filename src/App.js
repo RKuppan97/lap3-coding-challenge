52c322b157ff909fdc3a874e0df19177b955a5b1
“AdilIqbal95” <adil.iqbal@hotmail.co.uk>
2021-04-20 16:31:13 +0100

    Added header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header } from './components'
+//import { Header } from './components'
 import { Home, Results } from './pages';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
---
+++ src/layout/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header () {
+
+  return(
+    <>
+      <h1>Github Clone</h1>
+    </>
+  )
+
+}
+
+export default Header
